93c1cd182cd5fb9fa8e132f3fda29b8446e5203a
Dawid Kurek <dawikur@gmail.com>
2018-02-13 19:58:54 +0100

    Refactor python

--- body/scale/lang/python/python.go
+++ body/scale/lang/python/python.go
@@ -10,32 +10,34 @@ import (
 	"github.com/dawikur/dragon/utils"
 )
 
+func getPythonVersion() string {
+	ver := utils.Version("python", "--version")
+	if len(ver) > 0 {
+		return ver
+	}
+
+	buffer := bytes.Buffer{}
+	separator := ""
+
+	ver = utils.Version("python2", "--version")
+	if len(ver) > 0 {
+		separator = " "
+		buffer.WriteString(ver)
+	}
+
+	ver = utils.Version("python3", "--version")
+	if len(ver) > 0 {
+		buffer.WriteString(separator)
+		buffer.WriteString(ver)
+	}
+
+	return buffer.String()
+}
+
 func Scale() body.Scale {
 	return body.ScaleFunc(
 		utils.IsFile("[a-zA-Z]*.py"),
 		config.Lang.Color,
 		config.Lang.Python,
-		func() string {
-			ver := utils.Version("python", "--version")
-			if len(ver) > 0 {
-				return ver
-			}
-
-			buffer := bytes.Buffer{}
-			separator := ""
-
-			ver = utils.Version("python2", "--version")
-			if len(ver) > 0 {
-				separator = " "
-				buffer.WriteString(ver)
-			}
-
-			ver = utils.Version("python3", "--version")
-			if len(ver) > 0 {
-				buffer.WriteString(separator)
-				buffer.WriteString(ver)
-			}
-
-			return buffer.String()
-		})
+		getPythonVersion)
 }
