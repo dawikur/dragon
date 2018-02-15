79253049c55ffac7bbd54ad32dccf9a626109c35
Dawid Kurek <dawikur@gmail.com>
2018-02-15 15:12:59 +0100

    Add first vcs test

--- body/scale/vcs/vcs_test.go
+++ body/scale/vcs/vcs_test.go
@@ -3,8 +3,40 @@
 package vcs_test
 
 import (
+	"bytes"
 	"testing"
+
+	"github.com/dawikur/dragon/body"
+	"github.com/dawikur/dragon/body/scale/vcs"
+	"github.com/dawikur/dragon/config"
+	"github.com/dawikur/dragon/test"
 )
 
-func Test_TODO(t *testing.T) {
+func TestCVS(t *testing.T) {
+
+	for _, c := range []struct {
+		description string
+		info        string
+		parseStatus func(string) string
+		parseRepo   func(string) body.Mark
+		parseBranch func(string) string
+		isVisible   bool
+		content     string
+	}{
+		{"If invisible don't render",
+			"",
+			func(string) string { return "" },
+			func(string) body.Mark { return body.Mark{} },
+			func(string) string { return "" },
+			false,
+			""}} {
+
+		expected := body.Scale{
+			IsVisible:  c.isVisible,
+			Color:      config.Seg[2],
+			RenderImpl: func(buffer *bytes.Buffer) { buffer.WriteString(c.content) }}
+
+		scale := vcs.Scale(c.info, c.parseStatus, c.parseRepo, c.parseBranch)
+		test.CheckScale(t, c.description, expected, scale)
+	}
 }
